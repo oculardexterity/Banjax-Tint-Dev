1225fefb1755393bf8ffec1ee01d835e90d28bc4
Richard Hadden <oculardexterity@gmail.com>
2017-03-19 13:04:56 +0000

    basic working, refactor needed

--- module_assets/js/banjax-tint.js
+++ module_assets/js/banjax-tint.js
@@ -1,6 +1,8 @@
 
 function buildDataStructure() {
-	data =  $('textarea#test').val();
+	// Nothing here yet.
+
+  data = '';
 	return data;
 }
 
@@ -19,43 +21,164 @@ $('button#submit').click(function(e) {
         function(response) {console.log("BAD REQUEST:", response.responseText)})
 });
 
-var item = '<li class="ui-state-default draggable resizable"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><textarea>Type in me</textarea></li>'
 
 
+
+var item = '<li class="ui-state-default draggable resizable"><div class="toolbar"> <button href="#" class="button small" data-command="foreign">For</button> <button href="#" class="button small" data-command="addition">Add</button> </div> <div class="editor" contenteditable="true"> Try making some changes here. Add your own text or maybe an image.&nbsp; </div> </li>';
+
+
+
+// Build all sortable things
+// Buiild this properly into some initialistion function
 $( function() {
-    $( "#sortable" ).sortable({cancel : '.editable'});
+    $( "#sortable" ).sortable({cancel : '.editor'});
   	$(".resizable").resizable()
 
 
   	$('#main_add_item').click(function() {
-  		$('#sortable').append(item);
+  		var tb = $(item);
+
+      $('#sortable').append(tb);
+
+      tb.find('.toolbar button').click(bindToolbarButtons);
+      console.log();
   	});
 
 
-  } );
+} );
 
 
-function add_tags()
-{
-  
 
-   document.execCommand("insertHTML", false, "<span class='foreign'>"+ document.getSelection()+"</span>");
-
-   
-}
-
+// Not necessary I don't think?
 function remove_tags() {
 	document.execCommand("RemoveFormat", false, null);
 
 }
 
-function get_content(div) {
 
-	var content = $(div).html();
-	console.log(content);
-	content = content.replace('</div><div>', '<lb/>');
-	content = content.replace('<div>', '').replace('</div>', '');
-	console.log(content);
 
+function bindToolbarButtons(e) {
+     
+  // Grab command type from data-command attribute of element.   
+  var command = $(this).data('command');
+   
+
+   // This should not be necessary.
+  if (command == 'h1' || command == 'h2' || command == 'p') {
+    document.execCommand('formatBlock', false, command);
+  }
+   
+//BUSINESS SECTION
+   // Find a better way to do this generically?
+  if (command == 'foreign' || command == 'add' || command == 'del' || command == 'unclear' || command == 'u'
+    || command == 'sup' || command == 'sic') {
+    console.log(getSelectedNode().nodeName);
+
+    // Case where element already applied
+    if (getSelectedNode().nodeName == command.toUpperCase()) {
+      var node = getSelectedNode();
+      var contents = $(node).contents();
+      $(node).replaceWith(contents);
+      //Resets the buttons after removal.
+      $('button[data-command]').removeClass('hollow');
+    }
+
+    // Adds new element provided something is selected (length check)
+     else if (getSelectionHtml().length > 0) {
+        document.execCommand("insertHTML", false, "<"+command+">" + getSelectionHtml() +"</"+command+">");
+    }
+  }
+  if (command == 'gap') {
+    var sel = document.getSelection();
+      range = sel.getRangeAt(0);
+      newNode = document.createElement("gap");
+      newNode.appendChild(document.createTextNode(' '));
+      range.insertNode(newNode);
+  }
+   
+  // Do generic button... probably not worth it? 
+  //else document.execCommand($(this).data('command'), false, null);
+   
 }
+
+
+
+
+
+$('.toolbar button').click(bindToolbarButtons);
+
+
+// Adds <br> on enter press.
+$('div[contenteditable]').keydown(function(e) {
+    // trap the return key being pressed
+    e = e || window.event;
+    if (e.keyCode === 13) {
+
+      var sel = document.getSelection();
+      range = sel.getRangeAt(0);
+      newNode = document.createElement("br");
+			range.insertNode(newNode);
+		
+            //move the cursor
+      range.setStartAfter(newNode);
+			range.setEndAfter(newNode); 
+			sel.removeAllRanges();
+			sel.addRange(range);
+
+      return false;
+  }
+});
+
+
+
+
+
+
+// GET SELECTED TEXT/ELEMENTS
+var getSelectionHtml = function() {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerHTML;
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            html = document.selection.createRange().htmlText;
+        }
+    }
+    return html;
+}
+
+// THIS IS AN EVENT LISTENER... put it somewhere else
+document.onselectionchange = function() { //console.log(); 
+	   var parentNode = getSelectedNode().nodeName.toLowerCase();
+     $('button[data-command]').removeClass('hollow');
+  	 $('button[data-command="'+ parentNode +'"]').addClass('hollow');
+
+};
+
+
+// Gets the node in which caret rests... RETURNS NODE 
+var getSelectedNode = function() {
+    var node,selection;
+    if (window.getSelection) {
+      selection = getSelection();
+      node = selection.anchorNode;
+    }
+    if (!node && document.selection) {
+        selection = document.selection
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        node = range.commonAncestorContainer ? range.commonAncestorContainer :
+               range.parentElement ? range.parentElement() : range.item(0);
+    }
+    if (node) {
+      return (node.nodeName == "#text" ? node.parentNode : node);
+    }
+};
+
 
